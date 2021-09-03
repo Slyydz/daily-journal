e000bd7c9a641bfe21a948e9fff59310d08cef51
Brady Williams <williams.cole29@yahoo.com>
2021-09-03 16:22:20 -0500

    added funcionaitly to making posts to journal(needs refining not always posting)

---
+++ scripts/DataManager.js
@@ -0,0 +1,19 @@
+export const getPosts = () => {
+    return fetch("http://localhost:8088/posts")
+    .then(response => response.json())
+    .then(parsedResponse => {
+        return parsedResponse;
+    })
+}
+
+export const createPost = postObj => {
+    return fetch("http://localhost:8088/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+  
+    })
+        .then(response => response.json())
+  }

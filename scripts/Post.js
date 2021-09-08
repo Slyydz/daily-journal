6a842632c4ffeb1af2b2bfa0373ae26e149cb60f
Brady Williams <williams.cole29@yahoo.com>
2021-09-08 11:07:59 -0500

    Push to keep function as is

--- scripts/Post.js
+++ scripts/Post.js
@@ -1,12 +1,14 @@
 export const Post = (entry) => {
     const dateObj = new Date(entry.date);
-    const formattedDate = dateObj.toDateString();
+    const formattedDate = dateObj.toLocaleDateString();
     return `
     <section id="entry--${entry.id}" class="journalEntry">
-    <h4>User Id: ${entry.userId}</h4>
     <h3>${entry.concepts}</h3>
     <p>${entry.journalEntry}</p>
     <p>${formattedDate}</p>
+    <p>Mood: ${entry.moodId} </p>
+    <h4>User Id: ${entry.userId}</h4>
+    
 </section>
     `
   }
